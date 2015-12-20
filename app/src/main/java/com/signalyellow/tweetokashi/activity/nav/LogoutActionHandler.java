67595fc7d7fcea3478b7df86ec69bf6048e0fe80
shohei <room_shoheiyamamoto@yahoo.co.jp>
2015-12-20 19:39:45 +0900

    logout dialog

--- app/src/main/java/com/signalyellow/tweetokashi/activity/nav/LogoutActionHandler.java
+++ app/src/main/java/com/signalyellow/tweetokashi/activity/nav/LogoutActionHandler.java
@@ -1,7 +1,9 @@
 package com.signalyellow.tweetokashi.activity.nav;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import com.signalyellow.tweetokashi.activity.MainActivity;
@@ -11,11 +13,46 @@ import com.signalyellow.tweetokashi.twitter.TwitterUtils;
 public class LogoutActionHandler implements ItemActionHandler<Void> {
     @Override
     public boolean handle(Context context,Void entity ) {
-        TwitterUtils.deleteAccessToken(context);
-        Toast.makeText(context,"ログアウトしました",Toast.LENGTH_LONG).show();
-        TweetOkashiApplication app = (TweetOkashiApplication)context.getApplicationContext();
-        app.getHomeActivity().finish();
-        context.startActivity(new Intent(context, MainActivity.class));
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("確認　--　ログアウト")
+                .setCancelable(true)
+                .setMessage("ログアウトします")
+                .setPositiveButton("Yes", new PositiveButtonListener(context))
+                .setNegativeButton("Cancel", new NegativeButtonListener(context));
+        builder.create().show();
+
+
         return true;
+    }
+
+    class PositiveButtonListener implements DialogInterface.OnClickListener{
+
+        private Context context;
+
+        public PositiveButtonListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            TwitterUtils.deleteAccessToken(context);
+            Toast.makeText(context,"ログアウトしました",Toast.LENGTH_SHORT).show();
+
+
+            context.startActivity(new Intent(context, MainActivity.class));
+        }
+    }
+
+    class NegativeButtonListener implements DialogInterface.OnClickListener{
+        private Context context;
+
+        public NegativeButtonListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+
+        }
     }
 }
